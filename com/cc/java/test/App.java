d3042f98cf2cef1f58a1e1f65801bfff4f058c73
NightChill3R <91180420+Torsten0178@users.noreply.github.com>
2025-04-07 08:18:41 +0000

    blubb

--- com/cc/java/test/App.java
+++ com/cc/java/test/App.java
@@ -10,7 +10,7 @@ public class App
         System.out.println("Hello, World!");
 
         Helper.output("hi");
-        // Helper.output(Integer.toString(2));
+        Helper.output(Integer.toString(2));
         // Helper.output(Integer.toString(2));
 
     }
